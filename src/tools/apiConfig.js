9a024fb64e91fd2493ef6cdd5164e9a4dd9a486d
Tanuj Upreti <tanuj.upreti@smartdatainc.net>
2018-03-09 20:51:30 +0530

    finalll

--- src/tools/apiConfig.js
+++ src/tools/apiConfig.js
@@ -1,13 +1,13 @@
 
 //constants for api URL
 // var BASEURL = "http://172.10.233.57:6767/";
-var BASEURL = "http://172.10.196.63:6767/";
+var BASEURL = "https://notetakingapptask.herokuapp.com/";
 // var BASEURL = url.PRODUCTION_API_URL;
 
-export const SIGN_UP = BASEURL + 'users/register/';
-export const SIGN_IN = BASEURL + 'users/login/';
-export const ADD_NOTE = BASEURL + 'notes/addNote/';
-export const GET_NOTES = BASEURL + 'notes/getallNotes/';
-export const DELETE_NOTE = BASEURL + 'notes/deleteNote/';
-export const UPDATE_NOTE = BASEURL + 'notes/updateNote/';
-export const GET_NOTE_DETAIL = BASEURL + 'notes/getNoteById/';
+export const SIGN_UP = BASEURL + 'users/register';
+export const SIGN_IN = BASEURL + 'users/login';
+export const ADD_NOTE = BASEURL + 'notes/addNote';
+export const GET_NOTES = BASEURL + 'notes/getallNotes';
+export const DELETE_NOTE = BASEURL + 'notes/deleteNote';
+export const UPDATE_NOTE = BASEURL + 'notes/updateNote';
+export const GET_NOTE_DETAIL = BASEURL + 'notes/getNoteById';
